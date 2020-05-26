91bd3b304f1e3a5b3413ecb9aa96b8d049c8ddd2
Otávio Fonseca - DTI <otavio.fonseca@dtidigital.com.br>
2020-05-26 14:41:38 -0300

    Ajuste diretórios

---
+++ src/main/java/br/api/container/example/apicontainerexamplejava/DemoApplication.java
@@ -0,0 +1,13 @@
+package br.api.container.example.apicontainerexamplejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
